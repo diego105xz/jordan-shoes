9045303d1c52d1a3948aabd6f32b9f8a9759db03
Diego Melo <diegoo-melo@uni9.edu.br>
2023-03-02 23:01:59 -0300

    Topo e banner finalizado

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,17 @@
+import './style.css';
+
+
+function Banner (){
+    return(
+        <section >
+            <div className='banner'></div>
+            <div className='banner-texto'>
+                <h2>A melhor loja de Jordan</h2>
+                <p>O tênis Jordan é fruto de uma velha e forte parceria</p>
+                <p>entre a Nike e o jogador Michael Jordan.</p>
+            </div>
+        </section>
+    );
+}
+
+export default Banner;
---
+++ src/componentes/Topo/index.js
@@ -0,0 +1,17 @@
+import './style.css';
+
+function Topo(){
+    return(
+        <header>
+
+            <div className='topo1'>
+                <p>Ganhe R$ 10,00 de desconto no frente</p>
+            </div>
+            <div className='topo2'>
+                <h1>JordanShoes</h1>
+            </div>
+        </header>
+    );
+}
+
+export default Topo;
